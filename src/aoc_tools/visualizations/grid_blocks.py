6f5bdc59abf1f466606c5c37d8e7c62233db1032
Javier López <javilopez@uvigo.es>
2022-12-27 21:25:29 +0100

    Define the _GridNDPlotter base class

--- src/aoc_tools/visualizations/grid_blocks.py
+++ src/aoc_tools/visualizations/grid_blocks.py
@@ -2,19 +2,19 @@
 """Create plots of geometrical regions composed of discrete regular cells."""
 
 # Standard library imports:
-from typing import TypeVar
+from typing import Iterable, TypeVar
 
 # Third party imports:
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
-import matplotlib.colors
+from matplotlib.colors import BASE_COLORS, to_rgb
 from matplotlib.figure import Figure
 from matplotlib.font_manager import FontProperties
 from matplotlib.patches import Patch
 import numpy
 
 # Local application imports:
-from aoc_tools.algorithms.grid_blocks import Cell2D
+from aoc_tools.algorithms.grid_blocks import CellND
 
 
 Scalar = TypeVar("Scalar", str, int, float)
@@ -34,7 +34,7 @@ class ValuePalette:
         """Translate possible colour names into tuples of RGB values."""
         for value, colour in palette.items():
             if isinstance(colour, str):
-                palette[value] = matplotlib.colors.to_rgb(c=colour)
+                palette[value] = to_rgb(c=colour)
         return palette
 
     def apply_palette(self, value_array: numpy.ndarray) -> numpy.ndarray:
@@ -48,21 +48,31 @@ class ValuePalette:
     @classmethod
     def from_values(cls, possible_values: list[Scalar]) -> "ValuePalette":
         """Create a new ValuePalette with default colours from all possible values."""
-        colour_list = iter(matplotlib.colors.BASE_COLORS.values())
+        colour_list = iter(BASE_COLORS.values())
         palette = {v: next(colour_list) for v in possible_values}
         return cls(value_map=palette)
 
 
-class Grid2DPlotter:
-    """Create a 2D mosaic-like plot from a list of Cell2D objects."""
-    def __init__(self, cells: list[Cell2D], empty_value: Scalar = 0,
+class _GridNDPlotter:
+    """Base class for plotting mosaic-like regions composed of regular cells."""
+    def __init__(self, cells: list[CellND], empty_value: Scalar = 0,
                  palette: dict[Scalar, str | RGB] = None, legend: bool = True):
         self._cells = cells
         self._legend = legend
         self._empty_value = empty_value
         self._values = sorted({cell.value for cell in cells} | {empty_value})
+        self._limits = self._build_coord_limits()
         self._palette = self._build_palette(palette=palette)
-        self._limits = self._get_coord_limits()
+
+    def _build_coord_limits(self) -> dict[str, tuple[int, ...]]:
+        """Find the lowest and highest values of the stored cells' coordinates."""
+        coord_limits = {}
+        coordinates = self._cells[0].coord_map.keys()
+        for c in coordinates:
+            sorted_c = sorted(self._cells, key=lambda cell: cell.coord_map[c])
+            min_c, max_c = sorted_c[0].coord_map[c], sorted_c[-1].coord_map[c]
+            coord_limits.update({c: (min_c, max_c)})
+        return coord_limits
 
     def _build_palette(self, palette: dict[Scalar, str | RGB] = None) -> ValuePalette:
         """Create a new ValuePalette instance from provided Palette or known values."""
@@ -70,54 +80,57 @@ class Grid2DPlotter:
             return ValuePalette.from_values(possible_values=self._values)
         return ValuePalette(value_map=palette)
 
-    def _get_coord_limits(self) -> dict[str, tuple[int, int]]:
-        """Find the lowest and highest values of the stored cells' coordinates."""
-        sorted_x = sorted(self._cells, key=lambda cell: cell.x)
-        sorted_y = sorted(self._cells, key=lambda cell: cell.y)
-        min_x, max_x = sorted_x[0].x, sorted_x[-1].x
-        min_y, max_y = sorted_y[0].y, sorted_y[-1].y
-        return dict(x=(min_x, max_x), y=(min_y, max_y))
-
-    def plot_xy(self):
-        """Plot the stored 2D cells in a regular tessellation of squares."""
+    def _plot_hv(self, h_coord: str, v_coord: str, **other_coord_values: int):
+        """Plot the stored cells along an HV plane."""
         fig, axe = plt.subplots()
-        self._draw_grid_values(axe=axe)
+        data_array = self._build_2d_array(h=h_coord, v=v_coord, **other_coord_values)
+        self._draw_grid_values(axe=axe, data_array=data_array)
         self._draw_grid_borders(axe=axe)
-        self._draw_labels(axe=axe)
+        self._draw_labels(axe=axe, h=h_coord, v=v_coord)
         if self._legend:
             self._draw_legend(fig=fig)
         fig.tight_layout(rect=(0, 0.01, 1, 0.95 if self._legend else 1))
         plt.show()
 
-    def _build_hv_labels(self) -> tuple[list[int], list[int]]:
-        """Prepare tick ranges for the horizontal- and vertical-axis."""
-        h_labels = list(range(self._limits["x"][0], self._limits["x"][1] + 1))
-        v_labels = list(range(self._limits["y"][0], self._limits["y"][1] + 1))[::-1]
-        return h_labels, v_labels
-
-    def _build_hv_shape(self) -> tuple[int, int]:
-        """Prepare the shape of the 2D array to be drawn."""
-        h_lim, v_lim = self._limits["x"], self._limits["y"]
-        return v_lim[1] - v_lim[0] + 1, h_lim[1] - h_lim[0] + 1
-
-    def _build_hv_value_map(self) -> dict[tuple[int, int], Scalar]:
-        """Prepare a mapping of cell hv coordinates to cell values."""
-        max_v, min_h = self._limits["y"][1], self._limits["x"][0]
-        return {(max_v - cell.y, cell.x - min_h): cell.value for cell in self._cells}
-
-    def _draw_grid_values(self, axe: Axes):
-        """Add a 2D square tessellation coloured according to the cell values."""
-        # Build 2D array of cell values:
+    def _build_2d_array(self, h: str, v: str, **other_coord_values: int):
+        """Prepare a 2D array with values of cells inside the target HV plane."""
+        # Build 2D array of empty values:
         value_type = type(self._cells[0].value)
         if value_type == str:
             value_type = object
-        shape = self._build_hv_shape()
+        shape = self._build_hv_shape(h=h, v=v)
         data_array = numpy.full(shape, fill_value=self._empty_value, dtype=value_type)
-        for index, value in self._build_hv_value_map().items():
+        # Add target cells' values to array:
+        target_cells = self._build_target_cells(**other_coord_values)
+        hv_value_map = self._build_hv_value_map(cells=target_cells, h=h, v=v)
+        for index, value in hv_value_map.items():
             data_array[index] = value
-        # Transform into array with RGB channel:
+        return data_array
+
+    def _draw_grid_values(self, axe: Axes, data_array: numpy.ndarray):
+        """Add a 2D square tessellation coloured according to the cell values."""
         rgb_array = self._palette.apply_palette(value_array=data_array)
         axe.imshow(rgb_array, origin="upper", aspect="equal")
+
+    def _build_hv_shape(self, h: str, v: str) -> tuple[int, int]:
+        """Prepare the shape of the 2D array to be drawn."""
+        h_lim, v_lim = self._limits[h], self._limits[v]
+        return v_lim[1] - v_lim[0] + 1, h_lim[1] - h_lim[0] + 1
+
+    def _build_target_cells(self, **other_coord_values: int) -> Iterable[CellND]:
+        """Filter in stored cells matching target values of non-HV coordinates."""
+        for cell in self._cells:
+            for c, value in other_coord_values.items():
+                if cell.coord_map[c] != value:
+                    continue
+            yield cell
+
+    def _build_hv_value_map(self, cells: Iterable[CellND], h: str, v: str) \
+            -> dict[tuple[int, int], Scalar]:
+        """Prepare a mapping of cell hv coordinates to cell values."""
+        max_v, min_h = self._limits[v][1], self._limits[h][0]
+        return {(max_v - cell.coord_map[v], cell.coord_map[h] - min_h): cell.value
+                for cell in cells}
 
     @staticmethod
     def _draw_grid_borders(axe: Axes):
@@ -125,15 +138,21 @@ class Grid2DPlotter:
         [spine.set_linewidth(3) for spine in axe.spines.values()]
         axe.grid(which="minor", color="black", linewidth=3)
 
-    def _draw_labels(self, axe: Axes):
+    def _draw_labels(self, axe: Axes, h: str, v: str):
         """Add ticks and text labels to the plot's spines."""
-        h_labels, v_labels = self._build_hv_labels()
+        h_labels, v_labels = self._build_hv_labels(h=h, v=v)
         axe.tick_params(width=3)
         axe.set_xticks(range(len(h_labels)), labels=h_labels, weight="bold", minor=False)
         axe.set_yticks(range(len(v_labels)), labels=v_labels, weight="bold", minor=False)
         axe.set_xticks(numpy.arange(-0.5, len(h_labels) + 0.5), minor=True)
         axe.set_yticks(numpy.arange(-0.5, len(v_labels) + 0.5), minor=True)
         axe.tick_params(which="minor", length=0)
+
+    def _build_hv_labels(self, h: str, v: str) -> tuple[list[int], list[int]]:
+        """Prepare tick ranges for the horizontal- and vertical-axis."""
+        h_labels = list(range(self._limits[h][0], self._limits[h][1] + 1))
+        v_labels = list(range(self._limits[v][0], self._limits[v][1] + 1))[::-1]
+        return h_labels, v_labels
 
     def _draw_legend(self, fig: Figure):
         """Add a legend with drawn cell values."""
@@ -147,5 +166,12 @@ class Grid2DPlotter:
         legend.set_in_layout(False)
 
     def _build_legend_patch(self, value) -> Patch:
-        """Prepare a Patch artist using the palette colour of the provided value."""
+        """Prepare a Patch artist filled with the palette colour for the target value."""
         return Patch(facecolor=self._palette[value], edgecolor="black", linewidth=2)
+
+
+class Grid2DPlotter(_GridNDPlotter):
+    """Create a 2D mosaic-like plot from a list of XY cell objects."""
+    def plot_xy(self):
+        """Plot the stored 2D cells in a regular tessellation of squares."""
+        self._plot_hv(h_coord="x", v_coord="y")
--- tests/visualizations/tests_grid_blocks.py
+++ tests/visualizations/tests_grid_blocks.py
@@ -5,10 +5,12 @@
 import unittest
 
 # Local application imports:
-from aoc_tools.visualizations.grid_blocks import Cell2D, Grid2DPlotter
+# noinspection PyProtectedMember
+from aoc_tools.visualizations.grid_blocks import CellND, _GridNDPlotter
+from aoc_tools.visualizations.grid_blocks import Grid2DPlotter
 
 
-class Grid2DTests(unittest.TestCase):
+class BaseFeaturesTests(unittest.TestCase):
     def setUp(self) -> None:
         """Prepare the objects to be used during tests."""
         self.cell_params = [
@@ -19,31 +21,52 @@ class Grid2DTests(unittest.TestCase):
 
     def test_plot_default(self):
         """Assert a valid plot can be built providing only the list of cells."""
-        cells = [Cell2D(x=x, y=y, value=v) for x, y, v in self.cell_params]
-        plotter = Grid2DPlotter(cells=cells)
-        plotter.plot_xy()
+        cells = [CellND(x=x, y=y, value=v) for x, y, v in self.cell_params]
+        plotter = _GridNDPlotter(cells=cells)
+        plotter._plot_hv(h_coord="x", v_coord="y")
 
     def test_non_matching_empty_value(self):
         """Assert the default empty value can be replaced with a custom one."""
-        cells = [Cell2D(x=x, y=y, value=v) for x, y, v in self.cell_params]
-        plotter = Grid2DPlotter(cells=cells, empty_value=-10)
-        plotter.plot_xy()
+        cells = [CellND(x=x, y=y, value=v) for x, y, v in self.cell_params]
+        plotter = _GridNDPlotter(cells=cells, empty_value=-10)
+        plotter._plot_hv(h_coord="x", v_coord="y")
 
     def test_string_cell_values(self):
         """Assert cells with string values can be successfully plotted."""
         value_map = {0: "Alpha", 1: "Beta", 2: "Gamma"}
-        cells = [Cell2D(x=x, y=y, value=value_map[v]) for x, y, v in self.cell_params]
-        plotter = Grid2DPlotter(cells=cells, empty_value=value_map[0])
-        plotter.plot_xy()
+        cells = [CellND(x=x, y=y, value=value_map[v]) for x, y, v in self.cell_params]
+        plotter = _GridNDPlotter(cells=cells, empty_value=value_map[0])
+        plotter._plot_hv(h_coord="x", v_coord="y")
 
     def test_custom_colour_palette(self):
         """Assert the default value-colour map can be replaced with a custom one."""
-        cells = [Cell2D(x=x, y=y, value=v) for x, y, v in self.cell_params]
-        plotter = Grid2DPlotter(cells=cells, palette=self.palette)
-        plotter.plot_xy()
+        cells = [CellND(x=x, y=y, value=v) for x, y, v in self.cell_params]
+        plotter = _GridNDPlotter(cells=cells, palette=self.palette)
+        plotter._plot_hv(h_coord="x", v_coord="y")
 
     def test_hidden_legend(self):
         """Assert the legend can be hidden, and the Axes bbox expands accordingly."""
-        cells = [Cell2D(x=x, y=y, value=v) for x, y, v in self.cell_params]
-        plotter = Grid2DPlotter(cells=cells, legend=False)
+        cells = [CellND(x=x, y=y, value=v) for x, y, v in self.cell_params]
+        plotter = _GridNDPlotter(cells=cells, legend=False)
+        plotter._plot_hv(h_coord="x", v_coord="y")
+
+    def test_different_coord_names(self):
+        """Assert the names of the HV coordinates may be different from the usual XY."""
+        cells = [CellND(n=x, m=y, value=v) for x, y, v in self.cell_params]
+        plotter = _GridNDPlotter(cells=cells)
+        plotter._plot_hv(h_coord="n", v_coord="m")
+
+
+class Grid2DTests(unittest.TestCase):
+    def setUp(self) -> None:
+        """Prepare the objects to be used during tests."""
+        self.cell_params = [
+            (1, 1, 1), (1, 2, 1), (2, 4, 1), (3, 2, 1), (3, 4, 1), (4, 1, 1), (4, 2, 1),
+            (4, 5, 1), (2, 2, 1), (2, 3, 1), (5, 2, 1), (5, 3, 1), (5, 4, 1), (5, 5, 1),
+            (6, 4, 1), (7, 2, 1), (3, 3, 2), (4, 3, 2), (4, 4, 2)]
+
+    def test_plot_xy(self):
+        """Assert the X and Y coordinates are used as HV plot coordinates."""
+        cells = [CellND(x=x, y=y, value=v) for x, y, v in self.cell_params]
+        plotter = Grid2DPlotter(cells=cells)
         plotter.plot_xy()
