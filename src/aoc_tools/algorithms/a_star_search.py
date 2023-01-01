05f916cb2f90996091434b5bebab00830630b8f4
Javier López <javilopez@uvigo.es>
2023-01-01 19:50:20 +0100

    Rename A* search Node class to ASNode

--- src/aoc_tools/algorithms/a_star_search.py
+++ src/aoc_tools/algorithms/a_star_search.py
@@ -7,17 +7,17 @@ import heapq
 from typing import Callable, Iterable, Optional
 
 
-class Node(metaclass=abc.ABCMeta):
+class ASNode(metaclass=abc.ABCMeta):
     """ABC for an individual node in an A* search algorithm."""
     __slots__ = ["_parent"]
 
-    def __init__(self, parent: "Node" = None):
+    def __init__(self, parent: "ASNode" = None):
         self._parent = parent
 
     def __hash__(self) -> int:
         return hash(self.id)
 
-    def __lt__(self, other: "Node") -> bool:
+    def __lt__(self, other: "ASNode") -> bool:
         return self.f < other.f
 
     def __repr__(self) -> str:
@@ -26,29 +26,29 @@ class Node(metaclass=abc.ABCMeta):
     @property
     @abc.abstractmethod
     def id(self) -> str:
-        """Provide a string identifier unique to this Node."""
+        """Provide a string identifier unique to this ASNode."""
         raise NotImplementedError
 
     @property
     @abc.abstractmethod
     def g(self) -> int:
-        """Compute the cost for reaching this Node from the search start point."""
+        """Compute the cost for reaching this ASNode from the search start point."""
         raise NotImplementedError
 
     @property
     @abc.abstractmethod
     def h(self) -> int:
-        """Estimate the cost for reaching the search goal from this Node."""
+        """Estimate the cost for reaching the search goal from this ASNode."""
         raise NotImplementedError
 
     @abc.abstractmethod
-    def get_successors(self) -> Iterable["Node"]:
-        """List all nodes to search that are directly reachable from this Node."""
+    def get_successors(self) -> Iterable["ASNode"]:
+        """List all nodes to search that are directly reachable from this ASNode."""
         raise NotImplementedError
 
     @property
-    def parent(self) -> Optional["Node"]:
-        """Provide the parent node of this Node (or None, if no parent)."""
+    def parent(self) -> Optional["ASNode"]:
+        """Provide the parent node of this ASNode (or None, if no parent)."""
         return self._parent
 
     @property
@@ -57,13 +57,13 @@ class Node(metaclass=abc.ABCMeta):
         return self.g + self.h
 
     @property
-    def lineage(self) -> list["Node"]:
-        """List this Node and its recursive parents (if any)."""
+    def lineage(self) -> list["ASNode"]:
+        """List this ASNode and its recursive parents (if any)."""
         return [self] + (self.parent.lineage if self.parent else [])
 
 
-def a_star_search(start: Node, goal_func: Callable) -> Node:
-    """Find the path of lesser cost for reaching a goal objective from a start Node."""
+def a_star_search(start: ASNode, goal_func: Callable) -> ASNode:
+    """Find the path of lesser cost for reaching a goal objective from a start ASNode."""
     # Build lists / queues / min heaps / sets / cost maps:
     pending_nodes = [start]
     visited_nodes = set()
@@ -88,4 +88,4 @@ def a_star_search(start: Node, goal_func: Callable) -> Node:
         # Register the parent node's location as already seen:
         visited_nodes.add(q_node)
     # If code reaches this point, the goal was never reached:
-    raise ValueError("The search could not reach the end Node.")
+    raise ValueError("The search could not reach the end ASNode.")
