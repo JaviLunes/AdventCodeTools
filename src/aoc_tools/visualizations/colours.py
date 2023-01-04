db1dfc22a9e716792772a82ed64f97cc726e0075
Javier <javilopez@uvigo.es>
2023-01-04 12:49:55 +0100

    Make ValuePalette handle 255-based RGB colours

--- src/aoc_tools/visualizations/colours.py
+++ src/aoc_tools/visualizations/colours.py
@@ -11,23 +11,29 @@ import numpy
 
 Scalar = TypeVar("Scalar", str, int, float)
 RGB = tuple[float, float, float]
+RGB255 = tuple[int, int, int]
+Colour = str | RGB | RGB255
 
 
 class ValuePalette:
     """Map scalar values to specific RGB colours."""
-    def __init__(self, value_map: dict[Scalar, str | RGB]):
-        self._palette = self._translate_palette(palette=value_map)
+    def __init__(self, value_map: dict[Scalar, Colour]):
+        self._palette = {v: self._translate_colour(c) for v, c in value_map.items()}
 
     def __getitem__(self, value: Scalar) -> RGB:
         return self._palette[value]
 
     @staticmethod
-    def _translate_palette(palette: dict[Scalar, str | RGB]) -> dict[Scalar, RGB]:
-        """Translate possible colour names into tuples of RGB values."""
-        for value, colour in palette.items():
-            if isinstance(colour, str):
-                palette[value] = to_rgb(c=colour)
-        return palette
+    def _translate_colour(colour: Colour) -> RGB:
+        """Translate a compatible colour definition into a RGB value."""
+        if isinstance(colour, str):
+            return to_rgb(c=colour)
+        elif isinstance(colour, tuple) and len(colour) == 3:
+            if all(isinstance(v, int) and 0 <= v <= 255 for v in colour):
+                return colour[0] / 255, colour[1] / 255, colour[2] / 255
+            elif all(isinstance(v, float) and 0 <= v <= 1 for v in colour):
+                return colour
+        raise ValueError(f"The colour {colour} could not be translated.")
 
     def apply_palette(self, value_array: numpy.ndarray) -> numpy.ndarray:
         """Transform a nD array with values into a (n+1)D array with a colour channel."""
