fa44aae9ea1c8e0311b5568a3ab3e1995aaa80e0
Javier <javilopez@uvigo.es>
2023-01-24 10:17:07 +0100

    Move puzzle-building tools to the new 'build' subpackage

--- src/aoc_tools/__init__.py
+++ src/aoc_tools/__init__.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 """Shared tools used for building, managing and solving all Advent of Code projects."""
 
-from aoc_tools.puzzle_building import AdventBuilder
+from aoc_tools.build.puzzle_building import AdventBuilder
 from aoc_tools.puzzle_solving import AdventCalendar, AdventSolver, read_puzzle_input
 from aoc_tools import algorithms
---
+++ src/aoc_tools/build/__init__.py
@@ -0,0 +1,2 @@
+# coding=utf-8
+"""Tools used for building daily-puzzle files and AoC projects from templates."""
