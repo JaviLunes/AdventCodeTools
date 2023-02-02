06d5f13acdc5bde4b06404eed841efdb2bdbfa24
Javier <javilopez@uvigo.es>
2023-02-02 10:40:37 +0100

    Move the AdventCalendar class to its own module

--- src/aoc_tools/__init__.py
+++ src/aoc_tools/__init__.py
@@ -2,5 +2,6 @@
 """Shared tools used for building, managing and solving all Advent of Code projects."""
 
 from aoc_tools.build.puzzle_building import AdventBuilder
-from aoc_tools.puzzle_solving import AdventCalendar, AdventSolver, read_puzzle_input
+from aoc_tools.puzzle_solving import AdventSolver, read_puzzle_input
+from aoc_tools.project_calendar import AdventCalendar
 from aoc_tools import algorithms
